da9107db0a6794344fea5ca093c13304708a31a9
burakbuyuktombak <73587638+burakbuyuktombak@users.noreply.github.com>
2023-02-08 20:16:59 +0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -33,7 +33,7 @@ def clean_data(df):
     # drop the original categories column from 'df'
     df.drop(['categories'], axis=1, inplace=True)
     
-    # concatenate the original datafram with the new 'categories' dataframe
+    # concatenate the original dataframe with the new 'categories' dataframe
     df = pd.concat([df,categories], join='inner', axis=1)
     
     # drop duplicates
